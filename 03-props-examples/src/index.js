1bba3690c08de2290122ffd6410742bc16940c90
Saqib Amin <saqibamin257@gmail.com>
2020-08-12 12:46:45 +0500

    Exe-6 Communicating With Parent Components

--- 03-props-examples/src/index.js
+++ 03-props-examples/src/index.js
@@ -32,15 +32,37 @@ import ReactDOM from 'react-dom';
 
 //----------Example-5 Destruction 2nd way
 
-const Person = (props) =>{
-    const {firstname} = props;
-    const {lastname}= props;
+// const Person = (props) =>{
+//     const {firstname} = props;
+//     const {lastname}= props;
+//     return(
+//     <span>Hi, {firstname} {lastname}!</span>
+//     );
+// }
+
+//--------------Example-6
+// Communicating With Parent Components
+// Pass parameter from child to Parent
+//If a child needs to send data to its parent, the parent can send down a function as a prop, like this:
+
+function handleAction(event){
+    console.log('child did',event);
+}
+
+function Parent(){
     return(
-    <span>Hi, {firstname} {lastname}!</span>
+        <Child onAction={handleAction} />
+    );
+}
+
+function Child({onAction}){
+    return(
+        <button onClick={onAction}>Click</button>
     );
 }
 
 ReactDOM.render(
-    <Person firstname={"saqib"} lastname={"Amin"}/>,
+    // <Person firstname={"saqib"} lastname={"Amin"}/>,
+    <Parent/>,
     document.getElementById("root")
 );
