790349ec34dba86f3f291a7430f67df4db2816ac
Saqib Amin <saqibamin257@gmail.com>
2020-10-14 11:59:52 +0100

    03-props-examples

--- 03-props-examples/src/index.js
+++ 03-props-examples/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//Note
+//------- Run command "npm install" in terminal to install dependencies
+//--------"npm start"  to start the project
+//-----------Please Uncommment each exercise and run-----------//
+
 //-----------Example-1 Normal Function
 // function Person(props){
 //     return(
@@ -8,16 +13,31 @@ import ReactDOM from 'react-dom';
 //     );
 // }
 
+// ReactDOM.render(
+//     <Person name={"saqib amin"}/>,    
+//     document.getElementById("root")
+// );
+
 //----------Example-2 ES6-way
 // const Person = (props) => (
 //             <span>Hi {props.name}</span>
 //             );
+
+// ReactDOM.render(
+//     <Person name={"saqib amin"}/>,    
+//     document.getElementById("root")
+// );
 
 //----------Example-3 Destructuring
 
 // const Person =({name}) => (
 //                         <span>Hi {name}!</span>
 //                         );
+
+// ReactDOM.render(
+//     <Person name={"saqib amin"}/>,    
+//     document.getElementById("root")
+// );
 
 // //----------Example-4 Destruction 2nd way
 
@@ -30,39 +50,47 @@ import ReactDOM from 'react-dom';
 // }
 
 
+// ReactDOM.render(
+//     <Person firstname={"saqib"} lastname={"amin"}/>,    
+//     document.getElementById("root")
+// );
+
+
 //----------Example-5 Destruction 2nd way
 
-// const Person = (props) =>{
-//     const {firstname} = props;
-//     const {lastname}= props;
+// const Person = ({firstname,lastname}) =>{//     
 //     return(
 //     <span>Hi, {firstname} {lastname}!</span>
 //     );
 // }
+
+// ReactDOM.render(
+//     <Person firstname={"saqib"} lastname={"amin"}/>,    
+//     document.getElementById("root")
+// );
 
 //--------------Example-6
 // Communicating With Parent Components
 // Pass parameter from child to Parent
 //If a child needs to send data to its parent, the parent can send down a function as a prop, like this:
 
-function handleAction(event){
-    console.log('child did',event);
-}
+// function handleAction(event){
+//     console.log('child did',event);
+// }
 
-function Parent(){
-    return(
-        <Child onAction={handleAction} />
-    );
-}
+// function Parent(){
+//     return(
+//         <Child onAction={handleAction} />
+//     );
+// }
 
-function Child({onAction}){
-    return(
-        <button onClick={onAction}>Click</button>
-    );
-}
+// function Child({onAction}){
+//     return(
+//         <button onClick={onAction}>Click</button>
+//     );
+// }
 
-ReactDOM.render(
-    // <Person firstname={"saqib"} lastname={"Amin"}/>,
-    <Parent/>,
-    document.getElementById("root")
-);
+// ReactDOM.render(    
+//     <Parent/>,
+//     document.getElementById("root")
+// );
